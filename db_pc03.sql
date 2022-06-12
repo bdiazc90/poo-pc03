b25fb7f329e3d5f6b1f826d53a298df1d3118cc7
Bruno Díaz <bdiazc90@gmail.com>
2022-06-12 13:16:37 -0500

    Create db_pc03.sql

---
+++ db_pc03.sql
@@ -0,0 +1,44 @@
+-- ************************************** `equipo`
+CREATE TABLE `equipo`
+(
+ `pais`       varchar(100) NOT NULL ,
+ `continente` varchar(100) NOT NULL ,
+
+PRIMARY KEY (`pais`)
+);
+
+
+-- ************************************** `partido`
+CREATE TABLE `partido`
+(
+ `id`      int NOT NULL AUTO_INCREMENT ,
+ `pais_a`  varchar(100) NOT NULL ,
+ `goles_a` int NOT NULL ,
+ `goles_b` int NOT NULL ,
+ `pais_b`  varchar(100) NOT NULL ,
+
+PRIMARY KEY (`id`),
+KEY `FK_15` (`pais_b`),
+CONSTRAINT `FK_13` FOREIGN KEY `FK_15` (`pais_b`) REFERENCES `equipo` (`pais`),
+KEY `FK_20` (`pais_a`),
+CONSTRAINT `FK_18` FOREIGN KEY `FK_20` (`pais_a`) REFERENCES `equipo` (`pais`)
+);
+
+
+-- ************************************** `clasificacion`
+CREATE TABLE `clasificacion`
+(
+ `pais`         varchar(100) NOT NULL ,
+ `p_jugados`    int NOT NULL ,
+ `p_ganados`    int NOT NULL ,
+ `p_empatados`  int NOT NULL ,
+ `p_perdidos`   int NOT NULL ,
+ `puntos`       int NOT NULL ,
+ `goles_favor`  int NOT NULL ,
+ `goles_contra` int NOT NULL ,
+ `diff_goles`   int NOT NULL ,
+
+PRIMARY KEY (`pais`),
+KEY `FK_26` (`pais`),
+CONSTRAINT `FK_24` FOREIGN KEY `FK_26` (`pais`) REFERENCES `equipo` (`pais`)
+);
