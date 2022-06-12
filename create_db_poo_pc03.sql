2af1a687401d7d0f9ffb62cc7205a4c78959c9ff
Bruno Díaz <bdiazc90@gmail.com>
2022-06-12 15:21:14 -0500

    Update create_db_poo_pc03.sql

--- create_db_poo_pc03.sql
+++ create_db_poo_pc03.sql
@@ -3,40 +3,13 @@ CREATE DATABASE IF NOT EXISTS `poo_pc03` DEFAULT CHARACTER SET utf8 COLLATE utf8
 USE `poo_pc03`;
 
 -- ************************************** `equipo`
+
 CREATE TABLE `equipo`
 (
- `pais`       varchar(100) NOT NULL ,
- `continente` varchar(100) NOT NULL ,
-
-PRIMARY KEY (`pais`)
-);
-
-
--- ************************************** `partido`
-CREATE TABLE `partido`
-(
- `id`      int NOT NULL AUTO_INCREMENT ,
- `pais_a`  varchar(100) NOT NULL ,
- `goles_a` int NOT NULL ,
- `goles_b` int NOT NULL ,
- `pais_b`  varchar(100) NOT NULL ,
+ `id`         int NOT NULL AUTO_INCREMENT ,
+ `pais`       varchar(500) NOT NULL ,
+ `continente` varchar(500) NOT NULL ,
+ `puntos`     int NOT NULL ,
 
 PRIMARY KEY (`id`)
-);
-
-
--- ************************************** `clasificacion`
-CREATE TABLE `clasificacion`
-(
- `pais`         varchar(100) NOT NULL ,
- `p_jugados`    int NOT NULL ,
- `p_ganados`    int NOT NULL ,
- `p_empatados`  int NOT NULL ,
- `p_perdidos`   int NOT NULL ,
- `puntos`       int NOT NULL ,
- `goles_favor`  int NOT NULL ,
- `goles_contra` int NOT NULL ,
- `diff_goles`   int NOT NULL ,
-
-PRIMARY KEY (`pais`)
 );
